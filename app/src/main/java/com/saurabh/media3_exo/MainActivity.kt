bfaf42742ef0da87c6134df6dc1da482ebe82fa2
Prakash Ranjan <prakash.ranjan@nium.com>
2024-04-17 20:36:00 +0530

    Video is showing now and mediaSource factory added

--- app/src/main/java/com/saurabh/media3_exo/MainActivity.kt
+++ app/src/main/java/com/saurabh/media3_exo/MainActivity.kt
@@ -2,10 +2,124 @@ package com.saurabh.media3_exo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.OptIn
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.ui.PlayerControlView
+import androidx.media3.ui.PlayerView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    //http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4
+    private val bunnyUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+
+    private val playerView : PlayerView by lazy {
+        findViewById(R.id.playerview)
+    }
+
+    //in this project we are using default media source
+    private val mediaSourceFactory: MediaSource.Factory by lazy {
+        DefaultMediaSourceFactory(this)
+    }
+
+    private val player : ExoPlayer by lazy {
+        ExoPlayer.Builder(this).setMediaSourceFactory(mediaSourceFactory).build()
+    }
+
+    @OptIn(UnstableApi::class) override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mediaItem = MediaItem.fromUri(bunnyUrl)
+        player.setMediaItem(mediaItem)
+
+        //DRM mediaItem
+        /**
+         * val mediaItem =
+         *           MediaItem.Builder()
+         *             .setUri(videoUri)
+         *             .setDrmConfiguration(
+         *               MediaItem.DrmConfiguration.Builder(C.WIDEVINE_UUID)
+         *                 .setLicenseUri(licenseUri)
+         *                 .setMultiSession(true)
+         *                 .setLicenseRequestHeaders(httpRequestHeaders)
+         *                 .build()
+         *             )
+         *             .build()
+         */
+
+        //add Media source
+        /**
+         * Different media sources
+         * DashMediaSource for DASH.
+         * SsMediaSource for SmoothStreaming.
+         * HlsMediaSource for HLS.
+         * ProgressiveMediaSource for regular media files.
+         * RtspMediaSource for RTSP.
+         */
+
+
+
+        playerView.player = player
+        player.prepare()
+        player.playWhenReady = true
+
+        player.addListener(object : Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                Log.e("playerState", playbackState.toString())
+            }
+
+            override fun onDeviceVolumeChanged(volume: Int, muted: Boolean) {
+                super.onDeviceVolumeChanged(volume, muted)
+                Log.e("DeviceVolume", "$volume - isMuted =$muted")
+            }
+
+            override fun onEvents(player: Player, events: Player.Events) {
+                super.onEvents(player, events)
+
+                Log.e("PlayerEvents", events.toString())
+
+            }
+
+            override fun onVolumeChanged(volume: Float) {
+                super.onVolumeChanged(volume)
+                Log.e("Volume2", volume.toString())
+            }
+        })
+
+        //Add subtitle
+        /**
+         * val subtitle =
+         *   SubtitleConfiguration.Builder(subtitleUri)
+         *     .setMimeType(mimeType) // The correct MIME type (required).
+         *     .setLanguage(language) // The subtitle language (optional).
+         *     .setSelectionFlags(selectionFlags) // Selection flags for the track (optional).
+         *     .build()
+         * val mediaItem =
+         *   MediaItem.Builder().setUri(videoUri).setSubtitleConfigurations(listOf(subtitle)).build()
+         */
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        player.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        player.play()
+    }
+
+    override fun onStop() {
+        player.release()
+        super.onStop()
     }
 }
